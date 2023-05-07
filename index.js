9d3031a910b7065a4d9f4eebc7515aa091071f82
3laa <3laa.xvx@gmail.com>
2023-05-07 12:08:58 +0300

    0.3

--- index.js
+++ index.js
@@ -1,61 +1,109 @@
 // Defined variables
 let calculator = "";
+let calcDisplay = document.getElementById("calculatorDisplay");
+const num0Btn = document.getElementById("num0");
 const num1Btn = document.getElementById("num1");
 const num2Btn = document.getElementById("num2");
 const num3Btn = document.getElementById("num3");
+const num4Btn = document.getElementById("num4");
+const num5Btn = document.getElementById("num5");
+const num6Btn = document.getElementById("num6");
+const num7Btn = document.getElementById("num7");
+const num8Btn = document.getElementById("num8");
+const num9Btn = document.getElementById("num9");
 const plusBtn = document.getElementById("plusBtn");
-const minusBtn = document;
+const minusBtn = document.getElementById("minusBtn");
 const multiBtn = document.getElementById("multiBtn");
 const deviBtn = document.getElementById("deviBtn");
 const equalBtn = document.getElementById("equalBtn");
 const resetBtn = document.getElementById("resetBtn");
 
+num0Btn.addEventListener("click", function () {
+  calculator += "0";
+  calcDisplay.innerText = calculator;
+});
+
 num1Btn.addEventListener("click", function () {
-  calculator += " 1 ";
-  console.log(calculator);
+  calculator += "1";
+  calcDisplay.innerText = calculator;
 });
 
 num2Btn.addEventListener("click", function () {
-  calculator += " 2 ";
-  console.log(calculator);
+  calculator += "2";
+  calcDisplay.innerText = calculator;
 });
 
 num3Btn.addEventListener("click", function () {
-  calculator += " 3 ";
-  console.log(calculator);
+  calculator += "3";
+  calcDisplay.innerText = calculator;
 });
+
+num4Btn.addEventListener("click", function () {
+  calculator += "4";
+  calcDisplay.innerText = calculator;
+});
+
+num5Btn.addEventListener("click", function () {
+  calculator += "5";
+  calcDisplay.innerText = calculator;
+});
+
+num6Btn.addEventListener("click", function () {
+  calculator += "6";
+  calcDisplay.innerText = calculator;
+});
+
+num7Btn.addEventListener("click", function () {
+  calculator += "7";
+  calcDisplay.innerText = calculator;
+});
+
+num8Btn.addEventListener("click", function () {
+  calculator += "8";
+  calcDisplay.innerText = calculator;
+});
+
+num9Btn.addEventListener("click", function () {
+  calculator += "9";
+  calcDisplay.innerText = calculator;
+});
+
 plusBtn.addEventListener("click", function () {
   if (calculator) {
     calculator += " + ";
-    console.log(calculator);
+    calcDisplay.innerText = calculator;
   } else {
     console.log("enter a number");
   }
 });
+
 multiBtn.addEventListener("click", function () {
   if (calculator) {
     calculator += " * ";
-    console.log(calculator);
+    calcDisplay.innerText = calculator;
   } else {
     console.log("enter a number");
   }
 });
+
 deviBtn.addEventListener("click", function () {
   if (calculator) {
     calculator += " / ";
-    console.log(calculator);
+    calcDisplay.innerText = calculator;
   } else {
     console.log("enter a number");
   }
 });
+
 resetBtn.addEventListener("click", function () {
   calculator = "";
-  console.clear();
+  calcDisplay.innerText = 0;
 });
+
 equalBtn.addEventListener("click", function () {
   if (calculator) {
     calculator = eval(calculator);
-    console.log(calculator);
+    calcDisplay.innerText = calculator;
   } else {
     console.log("enter a number");
   }
